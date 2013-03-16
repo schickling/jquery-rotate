93d3909b6cacc05175c83b89a517cb1c2b209acc
schickling <schickling.j@gmai.com>
2013-03-16 11:58:52 +0100

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ jquery-rotate
 =============
 
 Very lightweight jquery rotate plugin using CSS 3 Transformation
+
+# Usage
+
+```javascript
+$('#element').rotate(90); // rotate by 90 degree
+$('#element').rotate(0); // random rotate
+```
---
+++ jquery.rotate.js
@@ -0,0 +1,33 @@
+/**
+ * jquery-rotate v0.1.0
+ *  Very lightweight jquery rotate plugin using CSS 3 Transformation
+ *  https://github.com/schickling/jquery-rotate
+ */
+(function ($) {
+
+	$.fn.extend({
+		rotate: function (deg) {
+
+			// cache dom element
+			var $this = $(this);
+
+			// make deg random if not set
+			if (!deg) {
+				deg = Math.floor(Math.random() * 359);
+			}
+
+			// rotate dom element
+			$this.css({
+				'-webkit-transform': 'rotate(' + deg + 'deg)',
+				'-moz-transform': 'rotate(' + deg + 'deg)',
+				'-ms-transform': 'rotate(' + deg + 'deg)',
+				'-o-transform': 'rotate(' + deg + 'deg)',
+				'transform': 'rotate(' + deg + 'deg)'
+			});
+
+			// make chainable
+			return $this;
+		}
+	});
+
+})(jQuery);
